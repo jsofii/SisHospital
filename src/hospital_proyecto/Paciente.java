a5382d48a03c53b7698755075d635ec738225c3d
USER <USER@HOME>
2016-11-26 21:45:37 -0500

    agreggue clase consultorio

--- src/hospital_proyecto/Paciente.java
+++ src/hospital_proyecto/Paciente.java
@@ -11,6 +11,73 @@ package hospital_proyecto;
  * @author USER
  */
 public class Paciente extends Persona{
+    private String nombre;
+     private String diagnostico;
+     private Medico medico;
+     private Turno turno;
+     private Seguro seguro;
+     private Receta receta;
+     private double cuenta;
+
+    @Override
+    public String getNombre() {
+        return nombre;
+    }
+
+    @Override
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDiagnostico() {
+        return diagnostico;
+    }
+
+    public void setDiagnostico(String diagnostico) {
+        this.diagnostico = diagnostico;
+    }
+
+    public Medico getMedico() {
+        return medico;
+    }
+
+    public void setMedico(Medico medico) {
+        this.medico = medico;
+    }
+
+    public Turno getTurno() {
+        return turno;
+    }
+
+    public void setTurno(Turno turno) {
+        this.turno = turno;
+    }
+
+    public Seguro getSeguro() {
+        return seguro;
+    }
+
+    public void setSeguro(Seguro seguro) {
+        this.seguro = seguro;
+    }
+
+    public Receta getReceta() {
+        return receta;
+    }
+
+    public void setReceta(Receta receta) {
+        this.receta = receta;
+    }
+
+    public double getCuenta() {
+        return cuenta;
+    }
+
+    public void setCuenta(double cuenta) {
+        this.cuenta = cuenta;
+    }
+    
+    
     
     
 }
